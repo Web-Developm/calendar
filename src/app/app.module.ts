295ce96fc00307be8893dbe5ce32b03fa3645f4f
satyakrishnajasti <satyajasti9@gmail.com>
2021-06-17 11:34:23 +0530

    calendar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarComponent } from './calendar/calendar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatCalendarCellCssClasses, MatCalendar } from '@angular/material/datepicker';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent implements OnInit {
+
+  @ViewChild('calendar', { static: true }) calendar!: MatCalendar<Date>;
+  selectedDate: any;
+  minDate: string | null = null;
+  datehighlight = ["2021-06-07T18:30:00.000Z", "2021-06-06T18:30:00.000Z", "2021-06-08T18:30:00.000Z"];
+
+
+
+  constructor() { }
+
+  dateClass() {
+    return (date: Date): MatCalendarCellCssClasses => {
+      console.log(date);
+      const highlightDate = this.datehighlight.map(strDate => new Date(strDate)).some(d => d.getDate() === date.getDate() && d.getMonth() === date.getMonth() && d.getFullYear() === date.getFullYear());
+      return highlightDate ? 'special-date': '';
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+}
