8e7ec417d232c0f43bbba0cdb02bb54676fb8332
satyakrishnajasti <satyajasti9@gmail.com>
2021-06-17 11:51:30 +0530

    changed

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -11,7 +11,7 @@ export class CalendarComponent implements OnInit {
   @ViewChild('calendar', { static: true }) calendar!: MatCalendar<Date>;
   selectedDate: any;
   minDate: string | null = null;
-  datehighlight = ["2021-06-07T18:30:00.000Z", "2021-06-06T18:30:00.000Z", "2021-06-08T18:30:00.000Z"];
+  datehighlight = ["2021-06-05T18:30:00.000Z", "2021-06-06T18:30:00.000Z", "2021-06-07T18:30:00.000Z", "2021-06-09T18:30:00.000Z", "2021-06-08T18:30:00.000Z", "2021-06-10T18:30:00.000Z", "2021-06-11T18:30:00.000Z"];
 
 
 
@@ -21,7 +21,7 @@ export class CalendarComponent implements OnInit {
     return (date: Date): MatCalendarCellCssClasses => {
       console.log(date);
       const highlightDate = this.datehighlight.map(strDate => new Date(strDate)).some(d => d.getDate() === date.getDate() && d.getMonth() === date.getMonth() && d.getFullYear() === date.getFullYear());
-      return highlightDate ? 'special-date': '';
+      return highlightDate ? 'special-date' : '';
     };
   }
 
